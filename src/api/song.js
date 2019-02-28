e4262fe2a53bbc07c528b88865d6adf9f9e54ab8
dajiangde <35095870+dajiangde@users.noreply.github.com>
2019-02-28 10:07:26 +0800

    Update song.js

--- src/api/song.js
+++ src/api/song.js
@@ -5,32 +5,12 @@ import { ERR_OK } from 'api/config'
 
 const debug = process.env.NODE_ENV !== 'production'
 
-export function getLyric(mid) {
-  const url = debug ? '/api/lyric' : 'http://ustbhuangyi.com/music/api/lyric'
-
-  const data = Object.assign({}, commonParams, {
-    songmid: mid,
-    platform: 'yqq',
-    hostUin: 0,
-    needNewCode: 0,
-    categoryId: 10000000,
-    pcachetime: +new Date(),
-    format: 'json'
-  })
-
-  return axios.get(url, {
-    params: data
-  }).then((res) => {
-    return Promise.resolve(res.data)
-  })
-}
 
 export function getSongsUrl(songs) {
   const url = debug ? '/api/getPurlUrl' : 'http://ustbhuangyi.com/music/api/getPurlUrl'
 
   let mids = []
   let types = []
-
   songs.forEach((song) => {
     mids.push(song.mid)
     types.push(0)
@@ -45,22 +25,21 @@ export function getSongsUrl(songs) {
     needNewCode: 1,
     uin: 0
   })
-
   return new Promise((resolve, reject) => {
     let tryTime = 3
 
     function request() {
       return axios.post(url, {
         comm: data,
-        req_0: urlMid
+        url_mid: urlMid
       }).then((response) => {
         const res = response.data
         if (res.code === ERR_OK) {
-          let urlMid = res.req_0
+          let urlMid = res.url_mid
           if (urlMid && urlMid.code === ERR_OK) {
             const info = urlMid.data.midurlinfo[0]
             if (info && info.purl) {
-              resolve(urlMid.data.midurlinfo)
+              resolve(res)
             } else {
               retry()
             }
@@ -89,7 +68,7 @@ function genUrlMid(mids, types) {
   const guid = getUid()
   return {
     module: 'vkey.GetVkeyServer',
-    method: 'CgiGetVkey',
+    method: "CgiGetVkey",
     param: {
       guid,
       songmid: mids,
