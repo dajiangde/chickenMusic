42de947d83a08c317e4d3d425eaf2fbe7fdfa453
dajiangde <35095870+dajiangde@users.noreply.github.com>
2019-02-28 10:31:13 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-Vue.use(Router)
+Vue.use(Router);
 
 const Recommend = () => import('components/recommend/recommend')
 const Singer = () => import('components/singer/singer')
@@ -12,55 +12,34 @@ const Disc = () => import('components/disc/disc')
 const TopList = () => import('components/top-list/top-list')
 const UserCenter = () => import('components/user-center/user-center')
 
+
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      redirect: '/recommend'
-    },
-    {
-      path: '/recommend',
-      component: Recommend,
-      children: [
-        {
-          path: ':id',
-          component: Disc
-        }
-      ]
-    },
-    {
-      path: '/singer',
-      component: Singer,
-      children: [
-        {
-          path: ':id',
-          component: SingerDetail
-        }
-      ]
-    },
-    {
-      path: '/rank',
-      component: Rank,
-      children: [
-        {
-          path: ':id',
-          component: TopList
-        }
-      ]
-    },
-    {
-      path: '/search',
-      component: Search,
-      children: [
-        {
-          path: ':id',
-          component: SingerDetail
-        }
-      ]
-    },
-    {
-      path: '/user',
-      component: UserCenter
-    }
-  ]
+	routes:[
+		{
+			path:'/',
+			redirect:'/recommend'
+		},
+		{
+			path:'/recommend',
+			component:Recommend
+		},
+		{
+			path:'/singer',
+			component:Singer,
+			children: [
+				{
+					path: ':id',
+					component: SingerDetail
+				}
+			]
+		},
+		{
+			path:'/rank',
+			component:Rank
+		},
+		{
+			path:'/search',
+			component:Search
+		}
+	]
 })
