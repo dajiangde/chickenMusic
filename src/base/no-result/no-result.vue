eca6390a0114d9de17104343d4b03eeddabfa46f
dajiangde <35095870+dajiangde@users.noreply.github.com>
2019-02-28 10:12:57 +0800

    Update no-result.vue

--- src/base/no-result/no-result.vue
+++ src/base/no-result/no-result.vue
@@ -1,19 +1,12 @@
 <template>
   <div class="no-result">
     <div class="no-result-icon"></div>
-    <p class="no-result-text">{{title}}</p>
+    <p class="no-result-text"></p>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  export default {
-    props: {
-      title: {
-        type: String,
-        default: ''
-      }
-    }
-  }
+  
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
