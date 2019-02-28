e569a063fb48fc86318e023fa3490dde9a7bbacd
dajiangde <35095870+dajiangde@users.noreply.github.com>
2019-02-28 10:06:37 +0800

    Update recommend.js

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -1,19 +1,19 @@
 import jsonp from 'common/js/jsonp'
-import {commonParams, options} from './config'
+import {commonParams,options} from 'api/config'
 import axios from 'axios'
 
 const debug = process.env.NODE_ENV !== 'production'
 
 export function getRecommend() {
   const url = 'https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg'
-
-  const data = Object.assign({}, commonParams, {
-    platform: 'h5',
-    uin: 0,
-    needNewCode: 1
+  
+  const data = Object.assign({},commonParams,{
+	  platform: 'h5',
+	  uin:0,
+	  needNewCode:1
   })
-
-  return jsonp(url, data, options)
+  return jsonp(url,data,options)
+	
 }
 
 export function getDiscList() {
@@ -30,27 +30,6 @@ export function getDiscList() {
     categoryId: 10000000,
     rnd: Math.random(),
     format: 'json'
-  })
-
-  return axios.get(url, {
-    params: data
-  }).then((res) => {
-    return Promise.resolve(res.data)
-  })
-}
-
-export function getSongList(disstid) {
-  const url = debug ? '/api/getCdInfo' : 'http://ustbhuangyi.com/music/api/getCdInfo'
-
-  const data = Object.assign({}, commonParams, {
-    disstid,
-    type: 1,
-    json: 1,
-    utf8: 1,
-    onlysong: 0,
-    platform: 'yqq',
-    hostUin: 0,
-    needNewCode: 0
   })
 
   return axios.get(url, {
