f58c0d8dc8b24022e39185e6661678f2aa037f94
dajiangde <35095870+dajiangde@users.noreply.github.com>
2019-02-28 10:32:51 +0800

    Update state.js

--- src/store/state.js
+++ src/store/state.js
@@ -1,19 +1,13 @@
-import { playMode } from 'common/js/config'
-import { loadSearch } from 'common/js/cache'
+import {playMode} from 'common/js/config'
 
 const state = {
-  singer: {},
-  playing: false,
-  fullScreen: false,
-  playlist: [],
-  sequenceList: [],
-  mode: playMode.sequence,
-  currentIndex: -1,
-  disc: {},
-  topList: {},
-  searchHistory: loadSearch(),
-  playHistory: [],
-  favoriteList: []
+	singer:{},
+	playing:false,
+	fullScreen: false,
+	playlist: [],
+	sequenceList: [],
+	mode: playMode.sequence,
+	currentIndex: -1
 }
 
 export default state
