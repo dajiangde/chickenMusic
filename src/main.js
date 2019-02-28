62aae2d1a1d306f5bb5478cb9981f01c59730a32
dajiangde <35095870+dajiangde@users.noreply.github.com>
2019-02-28 10:33:24 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,40 +1,22 @@
 import 'babel-polyfill'
-import 'common/js/hack'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import fastclick from 'fastclick'
-import VueLazyload from 'vue-lazyload'
 import store from './store'
-import { SET_PLAY_HISTORY, SET_FAVORITE_LIST } from './store/mutation-types'
-import { loadPlay, loadFavorite } from 'common/js/cache'
-import { processSongsUrl } from 'common/js/song'
-
+import fastclick from 'fastclick'
 import 'common/stylus/index.styl'
-
-/* eslint-disable no-unused-vars */
-// import vConsole from 'vconsole'
+import VueLazyLoad from 'vue-lazyload'
 
 fastclick.attach(document.body)
 
-Vue.use(VueLazyload, {
-  loading: require('common/image/default.png')
-})
-
-const historySongs = loadPlay()
-processSongsUrl(historySongs).then((songs) => {
-  store.commit(SET_PLAY_HISTORY, songs)
-})
-
-const favoriteSongs = loadFavorite()
-processSongsUrl(favoriteSongs).then((songs) => {
-  store.commit(SET_FAVORITE_LIST, songs)
+Vue.use(VueLazyLoad,{
+	loading: require('common/image/default.png')
 })
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  render: h => h(App),
   router,
-  store,
-  render: h => h(App)
+  store
 })
