37244db96ede19a0c715894ee7ddf5a2c0a11a72
dajiangde <35095870+dajiangde@users.noreply.github.com>
2019-02-28 10:12:19 +0800

    Update loading.vue

--- src/base/loading/loading.vue
+++ src/base/loading/loading.vue
@@ -4,15 +4,15 @@
     <p class="desc">{{title}}</p>
   </div>
 </template>
-<script type="text/ecmascript-6">
-  export default {
-    props: {
-      title: {
-        type: String,
-        default: '正在载入...'
-      }
-    }
-  }
+<script>
+ export default{
+	 props: {
+	 	title: {
+	 		type: String,
+	 		default:'加载中...' 
+	 	},
+	 },
+ }
 </script>
 <style scoped lang="stylus" rel="stylesheet/stylus">
   @import "~common/stylus/variable"
