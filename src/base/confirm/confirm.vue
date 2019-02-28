f7ab33db6db49f3dcbad879b489ec002f8d32fd6
dajiangde <35095870+dajiangde@users.noreply.github.com>
2019-02-28 10:08:57 +0800

    Update confirm.vue

--- src/base/confirm/confirm.vue
+++ src/base/confirm/confirm.vue
@@ -1,12 +1,12 @@
 <template>
   <transition name="confirm-fade">
-    <div class="confirm" v-show="showFlag" @click.stop>
+    <div class="confirm">
       <div class="confirm-wrapper">
         <div class="confirm-content">
-          <p class="text">{{text}}</p>
+          <p class="text"></p>
           <div class="operate">
-            <div @click="cancel" class="operate-btn left">{{cancelBtnText}}</div>
-            <div @click="confirm" class="operate-btn">{{confirmBtnText}}</div>
+            <div class="operate-btn left"></div>
+            <div class="operate-btn"></div>
           </div>
         </div>
       </div>
@@ -15,43 +15,7 @@
 </template>
 
 <script type="text/ecmascript-6">
-  export default {
-    props: {
-      text: {
-        type: String,
-        default: ''
-      },
-      confirmBtnText: {
-        type: String,
-        default: '确定'
-      },
-      cancelBtnText: {
-        type: String,
-        default: '取消'
-      }
-    },
-    data() {
-      return {
-        showFlag: false
-      }
-    },
-    methods: {
-      show() {
-        this.showFlag = true
-      },
-      hide() {
-        this.showFlag = false
-      },
-      cancel() {
-        this.hide()
-        this.$emit('cancel')
-      },
-      confirm() {
-        this.hide()
-        this.$emit('confirm')
-      }
-    }
-  }
+  
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
