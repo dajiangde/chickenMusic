c1823b73da67028277f529a8e5e575d8769e9fb3
dajiangde <35095870+dajiangde@users.noreply.github.com>
2019-02-28 10:32:22 +0800

    Update mutations.js

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,42 +1,27 @@
-import * as types from './mutation-types'
+import * as types from './mutation-type'
 
-const mutations = {
-  [types.SET_SINGER](state, singer) {
-    state.singer = singer
-  },
-  [types.SET_PLAYING_STATE](state, flag) {
-    state.playing = flag
-  },
-  [types.SET_FULL_SCREEN](state, flag) {
-    state.fullScreen = flag
-  },
-  [types.SET_PLAYLIST](state, list) {
-    state.playlist = list
-  },
-  [types.SET_SEQUENCE_LIST](state, list) {
-    state.sequenceList = list
-  },
-  [types.SET_PLAY_MODE](state, mode) {
-    state.mode = mode
-  },
-  [types.SET_CURRENT_INDEX](state, index) {
-    state.currentIndex = index
-  },
-  [types.SET_DISC](state, disc) {
-    state.disc = disc
-  },
-  [types.SET_TOP_LIST](state, topList) {
-    state.topList = topList
-  },
-  [types.SET_SEARCH_HISTORY](state, history) {
-    state.searchHistory = history
-  },
-  [types.SET_PLAY_HISTORY](state, history) {
-    state.playHistory = history
-  },
-  [types.SET_FAVORITE_LIST](state, list) {
-    state.favoriteList = list
-  }
+const mutations ={
+	[types.SET_SINGER](state,singer){
+		state.singer=singer
+	},
+	[types.SET_PLAYING](state,flag){
+		state.playing=flag
+	},
+	[types.SET_FULLSCREEN](state,flag){
+		state.fullScreen=flag
+	},
+	[types.SET_PLAYLIST](state,playlist){
+		state.playlist=playlist
+	},
+	[types.SET_SEQUENCELIST](state,sequenceList){
+		state.sequenceList=sequenceList
+	},
+	[types.SET_MODE](state,playMode){
+		state.mode=playMode
+	},
+	[types.SET_CURRENTINDEX](state,currentIndex){
+		state.currentIndex=currentIndex
+	}
 }
 
 export default mutations
