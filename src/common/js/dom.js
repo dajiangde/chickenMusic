ea055d182388def86b8028f5617da438fab7f155
dajiangde <35095870+dajiangde@users.noreply.github.com>
2019-02-28 10:20:32 +0800

    Update dom.js

--- src/common/js/dom.js
+++ src/common/js/dom.js
@@ -1,47 +1,48 @@
-export function hasClass(el, className) {
-  return el.classList.contains(className)
+export function addClass(el,className){
+	if(hasClass(el,className)){
+		return
+	}
+	el.className=el.className+' '+className
 }
 
-export function addClass(el, className) {
-  el.classList.add(className)
+export function hasClass(el,className){
+	const reg = new RegExp('(^|\\s)'+className+'(\\s|$)')
+	return reg.test(el.className)
+} 
+
+export function getData(el,name,value){
+	const prefix = 'data-'
+	if (value) {
+		return el.setAttribute(prefix + name, value)
+	}
+	return el.getAttribute(prefix + name)
 }
 
-export function getData(el, name, val) {
-  const prefix = 'data-'
-  if (val) {
-    return el.setAttribute(prefix + name, val)
-  }
-  return el.getAttribute(prefix + name)
-}
-
-let elementStyle = document.createElement('div').style
-
-let vendor = (() => {
-  let transformNames = {
-    webkit: 'webkitTransform',
-    Moz: 'MozTransform',
-    O: 'OTransform',
-    ms: 'msTransform',
-    standard: 'transform'
-  }
-
-  for (let key in transformNames) {
-    if (elementStyle[transformNames[key]] !== undefined) {
-      return key
-    }
-  }
-
-  return false
+let domStyle = document.createElement('div').style
+let vendor = (()=>{
+	const transform = {
+		'webkit':'webkitTransform',
+		'O':'oTransfrom',
+		'ms':'msTransform',
+		'Moz':'MozTransform',
+		'standard':'transform'
+	}
+	for (let key in transform) {
+		if(domStyle[transform[key]]!==undefined){
+			return key
+		}
+	}
+	return false;
 })()
 
-export function prefixStyle(style) {
-  if (vendor === false) {
-    return false
-  }
-
-  if (vendor === 'standard') {
-    return style
-  }
-
-  return vendor + style.charAt(0).toUpperCase() + style.substr(1)
+export function prefixStyle(style){
+	if (vendor === false) {
+	    return false
+	  }
+	
+	  if (vendor === 'standard') {
+	    return style
+	  }
+	
+	return vendor+style.charAt(0).toUpperCase()+style.substr(1)
 }
