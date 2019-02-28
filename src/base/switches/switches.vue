a1b7280f149a371ea72bf640488f2ebd8f961b1d
dajiangde <35095870+dajiangde@users.noreply.github.com>
2019-02-28 10:19:02 +0800

    Update switches.vue

--- src/base/switches/switches.vue
+++ src/base/switches/switches.vue
@@ -1,30 +1,13 @@
 <template>
   <ul class="switches">
-    <li class="switch-item" v-for="(item,index) in switches" :class="{'active':currentIndex === index}"
-        @click="switchItem(index)">
-      <span>{{item.name}} </span>
+    <li class="switch-item">
+      <span></span>
     </li>
   </ul>
 </template>
 
 <script type="text/ecmascript-6">
-  export default {
-    props: {
-      switches: {
-        type: Array,
-        default: []
-      },
-      currentIndex: {
-        type: Number,
-        default: 0
-      }
-    },
-    methods: {
-      switchItem(index) {
-        this.$emit('switch', index)
-      }
-    }
-  }
+ 
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
