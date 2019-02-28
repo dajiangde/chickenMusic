d83e7a0b1a2e99d7e270ecd7db2b774eff705e5c
dajiangde <35095870+dajiangde@users.noreply.github.com>
2019-02-28 10:29:18 +0800

    Update singer-detail.vue

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,65 +1,55 @@
 <template>
   <transition name="slide">
-    <music-list :title="title" :bg-image="bgImage" :songs="songs"></music-list>
+		<music-list :songs="songList" :singer="singer"></music-list>
   </transition>
 </template>
 
-<script type="text/ecmascript-6">
-  import MusicList from 'components/music-list/music-list'
-  import { getSingerDetail } from 'api/singer'
-  import { ERR_OK } from 'api/config'
-  import { createSong, isValidMusic, processSongsUrl } from 'common/js/song'
-  import { mapGetters } from 'vuex'
-
-  export default {
-    computed: {
-      title() {
-        return this.singer.name
-      },
-      bgImage() {
-        return this.singer.avatar
-      },
-      ...mapGetters([
-        'singer'
-      ])
-    },
-    data() {
-      return {
-        songs: []
-      }
-    },
-    created() {
-      this._getDetail()
-    },
-    methods: {
-      _getDetail() {
-        if (!this.singer.id) {
-          this.$router.push('/singer')
-          return
-        }
-        getSingerDetail(this.singer.id).then((res) => {
-          if (res.code === ERR_OK) {
-            processSongsUrl(this._normalizeSongs(res.data.list)).then((songs) => {
-              this.songs = songs
-            })
-          }
-        })
-      },
-      _normalizeSongs(list) {
-        let ret = []
-        list.forEach((item) => {
-          let {musicData} = item
-          if (isValidMusic(musicData)) {
-            ret.push(createSong(musicData))
-          }
-        })
-        return ret
-      }
-    },
-    components: {
-      MusicList
-    }
-  }
+<script>
+	import MusicList from 'components/music-list/music-list'
+	import {mapGetters} from 'vuex'
+	import {getSingerDetail} from 'api/singer'
+	import {ERR_OK} from 'api/config'
+	import {createSong, processSongsUrl,isValidMusic} from 'common/js/song'
+  export default{	
+		data() {
+			return {
+				songList: []
+			}
+		},
+		computed:{
+			...mapGetters(['singer'])
+		},
+		created() {
+			this._getSingerDetail(this.singer.id)
+		},
+		methods:{
+			_getSingerDetail(singerId){
+				if(!singerId){
+					this.$router.back()
+				}
+				getSingerDetail(singerId).then((res)=>{
+					if (res.code === ERR_OK) {
+						processSongsUrl(this._nomalizeSong(res.data.list)).then((songs) => {
+							this.songList = songs
+						})
+					}
+				})
+			},
+			_nomalizeSong(list){
+				let arr=[]
+				list.forEach((item,index)=>{
+					const {musicData}=item
+					if (isValidMusic(musicData)) {
+						arr.push(createSong(musicData))
+					}
+				})
+				return arr
+			}
+		},
+		components:{
+			MusicList
+		}
+	}
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
