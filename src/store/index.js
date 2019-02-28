4be55b8e5bed6a811693f0f4008fd05c6960563d
dajiangde <35095870+dajiangde@users.noreply.github.com>
2019-02-28 10:32:06 +0800

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -9,12 +9,11 @@ import createLogger from 'vuex/dist/logger'
 Vue.use(Vuex)
 
 const debug = process.env.NODE_ENV !== 'production'
-
 export default new Vuex.Store({
-  actions,
-  getters,
-  state,
-  mutations,
-  strict: debug,
-  plugins: debug ? [createLogger()] : []
+	actions,
+	getters,
+	state,
+	mutations,
+	strict:debug,
+	plugins:debug?[createLogger()]:[]
 })
