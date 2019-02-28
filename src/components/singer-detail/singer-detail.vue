f6a2994068c8190e5b59f6e9f95967991b0330f2
dajiangde <35095870+dajiangde@users.noreply.github.com>
2019-02-28 10:29:33 +0800

    Update singer-detail.vue

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,61 +1,53 @@
 <template>
-  <transition name="slide">
-		<music-list :songs="songList" :singer="singer"></music-list>
-  </transition>
+  
+    <ul class="suggest-list">
+      <li class="suggest-item">
+        <div class="icon">
+          <i></i>
+        </div>
+        <div class="name">
+          <p class="text"></p>
+        </div>
+      </li>
+    </ul>
+    <div class="no-result-wrapper">
+      
+    </div>
 </template>
 
-<script>
-	import MusicList from 'components/music-list/music-list'
-	import {mapGetters} from 'vuex'
-	import {getSingerDetail} from 'api/singer'
-	import {ERR_OK} from 'api/config'
-	import {createSong, processSongsUrl,isValidMusic} from 'common/js/song'
-  export default{	
-		data() {
-			return {
-				songList: []
-			}
-		},
-		computed:{
-			...mapGetters(['singer'])
-		},
-		created() {
-			this._getSingerDetail(this.singer.id)
-		},
-		methods:{
-			_getSingerDetail(singerId){
-				if(!singerId){
-					this.$router.back()
-				}
-				getSingerDetail(singerId).then((res)=>{
-					if (res.code === ERR_OK) {
-						processSongsUrl(this._nomalizeSong(res.data.list)).then((songs) => {
-							this.songList = songs
-						})
-					}
-				})
-			},
-			_nomalizeSong(list){
-				let arr=[]
-				list.forEach((item,index)=>{
-					const {musicData}=item
-					if (isValidMusic(musicData)) {
-						arr.push(createSong(musicData))
-					}
-				})
-				return arr
-			}
-		},
-		components:{
-			MusicList
-		}
-	}
+<script type="text/ecmascript-6">
+  
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-  .slide-enter-active, .slide-leave-active
-    transition: all 0.3s
+  @import "~common/stylus/variable"
+  @import "~common/stylus/mixin"
 
-  .slide-enter, .slide-leave-to
-    transform: translate3d(100%, 0, 0)
+  .suggest
+    height: 100%
+    overflow: hidden
+    .suggest-list
+      padding: 0 30px
+      .suggest-item
+        display: flex
+        align-items: center
+        padding-bottom: 20px
+      .icon
+        flex: 0 0 30px
+        width: 30px
+        [class^="icon-"]
+          font-size: 14px
+          color: $color-text-d
+      .name
+        flex: 1
+        font-size: $font-size-medium
+        color: $color-text-d
+        overflow: hidden
+        .text
+          no-wrap()
+    .no-result-wrapper
+      position: absolute
+      width: 100%
+      top: 50%
+      transform: translateY(-50%)
 </style>
