59d8633193058ff2e1377f4ef87b9733c1b5dbda
dajiangde <35095870+dajiangde@users.noreply.github.com>
2019-02-28 10:27:58 +0800

    Update rank.vue

--- src/components/rank/rank.vue
+++ src/components/rank/rank.vue
@@ -1,81 +1,26 @@
 <template>
-  <div class="rank" ref="rank">
-    <scroll :data="topList" class="toplist" ref="toplist">
+  <div class="rank">
       <ul>
-        <li @click="selectItem(item)" class="item" v-for="item in topList">
+        <li class="item">
           <div class="icon">
-            <img width="100" height="100" v-lazy="item.picUrl"/>
+            <img width="100" height="100"/>
           </div>
           <ul class="songlist">
-            <li class="song" v-for="(song,index) in item.songList">
-              <span>{{index + 1}}</span>
-              <span>{{song.songname}}-{{song.singername}}</span>
+            <li class="song">
+              <span></span>
+              <span></span>
             </li>
           </ul>
         </li>
       </ul>
-      <div class="loading-container" v-show="!topList.length">
+      <!--<div class="loading-container">
         <loading></loading>
-      </div>
-    </scroll>
-    <router-view></router-view>
+      </div>-->
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  import Scroll from 'base/scroll/scroll'
-  import Loading from 'base/loading/loading'
-  import {getTopList} from 'api/rank'
-  import {ERR_OK} from 'api/config'
-  import {playlistMixin} from 'common/js/mixin'
-  import {mapMutations} from 'vuex'
-
-  export default {
-    mixins: [playlistMixin],
-    created() {
-      this._getTopList()
-    },
-    data() {
-      return {
-        topList: []
-      }
-    },
-    methods: {
-      handlePlaylist(playlist) {
-        const bottom = playlist.length > 0 ? '60px' : ''
-
-        this.$refs.rank.style.bottom = bottom
-        this.$refs.toplist.refresh()
-      },
-      selectItem(item) {
-        this.$router.push({
-          path: `/rank/${item.id}`
-        })
-        this.setTopList(item)
-      },
-      _getTopList() {
-        getTopList().then((res) => {
-          if (res.code === ERR_OK) {
-            this.topList = res.data.topList
-          }
-        })
-      },
-      ...mapMutations({
-        setTopList: 'SET_TOP_LIST'
-      })
-    },
-    watch: {
-      topList() {
-        setTimeout(() => {
-          this.$Lazyload.lazyLoadHandler()
-        }, 20)
-      }
-    },
-    components: {
-      Scroll,
-      Loading
-    }
-  }
+  export default{}
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
